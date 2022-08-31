f66cd3faa46717c4730ad21e9c581ac9c4e4aa9a
neootavio <99883591+neootavio@users.noreply.github.com>
2022-08-31 14:07:07 -0300

    Update scrollreveal.js

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -16,24 +16,40 @@ var slideRight = {
 
 sr.reveal('.item1');
 
-sr.reveal('.item2', {delay:1000});
+sr.reveal('.item2', {delay:500});
 
-sr.reveal('.item3', {delay:1500});
+sr.reveal('.item3', {delay:600});
 
-sr.reveal('.item4', {delay:1800});
+sr.reveal('.item4', {delay:700});
 
-sr.reveal('.item5', {delay:500});
+sr.reveal('.item5', {delay:700});
 
-sr.reveal('.item6', {delay:1000});
+sr.reveal('.item6', {delay:600});
 
-sr.reveal('.item7', {delay:1500});
+sr.reveal('.item7', {delay:500});
 
-sr.reveal('.item8', {delay:1800});
+sr.reveal('.item8', {delay:600});
 
-sr.reveal('.item9', {delay:500});
+sr.reveal('.item9', {delay:700});
 
-sr.reveal('.item10', {delay:1000});
+sr.reveal('.item10', {delay:700});
 
-sr.reveal('.item11', {delay:1500});
+sr.reveal('.item11', {delay:600});
 
-sr.reveal('.item12', {delay:1800});
+sr.reveal('.item12', {delay:500});
+
+sr.reveal('.item13', {delay:600});
+
+sr.reveal('.item14', {delay:700});
+
+sr.reveal('.item15', {delay:700});
+
+sr.reveal('.item16', {delay:600});
+
+sr.reveal('.item17', {delay:500});
+
+sr.reveal('.item18', {delay:600});
+
+sr.reveal('.item19', {delay:700});
+
+sr.reveal('.item20', {delay:600});
