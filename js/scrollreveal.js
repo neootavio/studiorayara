0d9bf8a2f334ec1cc931428d29c9df8826338d30
neootavio <99883591+neootavio@users.noreply.github.com>
2022-08-31 14:00:48 -0300

    adc animações

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -12,10 +12,28 @@ var slideRight = {
     opacity: null
 };
 
-sr.reveal('.item1', slideUp);
+//sr.reveal('.item1', {rotate: {x: 0, y: 80, z:0}});
 
-sr.reveal('.item2', {rotate: {x: 0, y: 80, z:0}});
+sr.reveal('.item1');
 
-sr.reveal('.item3', slideUp);
+sr.reveal('.item2', {delay:1000});
 
-sr.reveal('.item4', slideRight);
+sr.reveal('.item3', {delay:1500});
+
+sr.reveal('.item4', {delay:1800});
+
+sr.reveal('.item5', {delay:500});
+
+sr.reveal('.item6', {delay:1000});
+
+sr.reveal('.item7', {delay:1500});
+
+sr.reveal('.item8', {delay:1800});
+
+sr.reveal('.item9', {delay:500});
+
+sr.reveal('.item10', {delay:1000});
+
+sr.reveal('.item11', {delay:1500});
+
+sr.reveal('.item12', {delay:1800});
